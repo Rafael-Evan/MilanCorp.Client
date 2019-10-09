ede717184492025c7a83e0ab1de8c48d911d60da
Rafael Evangelista <50708291+Rafael-Evan@users.noreply.github.com>
2019-10-09 17:51:13 -0300

    09/10/2019
    09/10/2019

--- src/app/_services/aniversariante.service.ts
+++ src/app/_services/aniversariante.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, EventEmitter, Output } from '@angular/core';
 import { HttpClient, HttpEventType, HttpEvent, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { FileUpload } from '../_models/FileUpload';
 declare var $: any;
 
 @Injectable({
@@ -13,5 +14,17 @@ export class AniversarianteService {
     private baseUrl = `${environment.apiUrl}aniversariante`;
 
     constructor(private http: HttpClient) { }
+
+    ImportarAniversariantes(fileToUpload: File) {
+        const formData: FormData = new FormData();
+        formData.append('fileKey', fileToUpload, fileToUpload.name);
+        return this.http
+            .post(this.baseUrl + '/ImportarAniversariantes', formData);
+    }
+
+    CadastrarAniversariante(model: any) {
+        return this.http
+            .post(`${this.baseUrl}/cadastrarAniversariante`, model);
+    }
 
 }
--- src/app/aniversariante/aniversariante.component.ts
+++ src/app/aniversariante/aniversariante.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AniversarianteService } from '../_services/aniversariante.service';
+import * as $ from 'jquery';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-aniversariante',
@@ -8,10 +11,73 @@ import { AniversarianteService } from '../_services/aniversariante.service';
 })
 export class AniversarianteComponent implements OnInit {
 
-  constructor() { }
+  cadastrarAniversarianteForm: FormGroup;
+  cadastrarAniversarianteListForm: FormGroup;
+  fileToUpload: File = null;
+  aniversariante: any;
+
+  handleFileInput(files: FileList) {
+    this.fileToUpload = files.item(0);
+  }
+
+  constructor(private authService: AniversarianteService
+    , public fb: FormBuilder
+    , private toastr: ToastrService) { }
 
   ngOnInit() {
+    this.validacao();
 
+    $('.divlista').hide();
+    $('.divmanual').hide();
+
+    $('input[id="Lista"]').change(function () {
+      if ($("input[id=Lista][value='Sim']").prop("checked", true)) {
+        $('.divlista').show();
+        $('.divmanual').hide();
+      }
+    });
+
+    $('input[id="Manual"]').change(function () {
+      if ($("input[id=Manual][value='Sim']").prop("checked", true)) {
+        $('.divmanual').show();
+        $('.divlista').hide();
+      }
+    });
+  }
+
+  validacao() {
+    this.cadastrarAniversarianteForm = this.fb.group({
+      title: [''],
+      start: ['']
+    });
+
+    this.cadastrarAniversarianteListForm = this.fb.group({
+      arquivo: ['']
+    });
+  }
+
+  cadastrarAniversariante() {
+    if (this.cadastrarAniversarianteForm.valid) {
+      this.aniversariante = Object.assign(this.cadastrarAniversarianteForm.value);
+      this.authService.CadastrarAniversariante(this.aniversariante).subscribe(
+        () => {
+          this.cadastrarAniversarianteForm.reset();
+          this.toastr.success('Aniversariante cadastro com sucesso!');
+        }, error => {
+          this.toastr.error('Erro ao cadastrar o aniversariante!');
+        }
+      );
+    }
+  }
+
+  AniversariantesUpload() {
+    this.authService.ImportarAniversariantes(this.fileToUpload).subscribe(data => {
+      // do something, if upload success
+      this.cadastrarAniversarianteListForm.reset();
+      this.toastr.success('Aniversariante importados com sucesso!');
+    }, error => {
+      this.toastr.error('Erro ao importar os aniversariantes!');
+    });
   }
 
 }
