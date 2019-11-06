05c2ed5a81da5ec6f06d05b200981585df9f27f6
Rafael Evangelista <50708291+Rafael-Evan@users.noreply.github.com>
2019-11-06 09:24:13 -0300

    06/11/2019
    06/11/2019

--- src/app/relatorio-de-leilao/relatorio-de-leilao.component.ts
+++ src/app/relatorio-de-leilao/relatorio-de-leilao.component.ts
@@ -36,31 +36,26 @@ export class RelatorioDeLeilaoComponent implements OnInit {
     this.comitenteService.ListarComitentes().subscribe((data) => {
       this.comitentes = data;
     });
+  }
 
-    var doc = new jsPDF('p','pt','a4');
-    var specialElementHandlers = {
-      '#editor': function (element, renderer) {
-        return true;
-      }
-    };
-
-    $('#btGerarPDF').click(function () {
-
-      var margins = {
-        top: 80,
-        bottom: 60,
-        left: 40,
-        width: 522
-    };
-
-
-
-      doc.fromHTML($('#conteudo').html(), 0, 200, {
-         'width': 570,
-         'elementHandlers': specialElementHandlers
-       });
-       doc.save('DiasDeLeiloes.pdf');
-    });
+  CriaPDF() {
+    var minhaTabela = document.getElementById('tabela').innerHTML;
+    var style = "<style>";
+    style = style + "table {width: 100%;font: 20px Calibri;}";
+    style = style + "table, th, td {border: solid 1px #DDD; border-collapse: collapse;";
+    style = style + "padding: 2px 3px;text-align: center;}";
+    style = style + "</style>";
+    // CRIA UM OBJETO WINDOW
+    var win = window.open('', '', 'height=700,width=700');
+    win.document.write('<html><head>');
+    win.document.write('<title>Empregados</title>');
+    win.document.write(style);
+    win.document.write('</head>');
+    win.document.write('<body>');
+    win.document.write(minhaTabela);
+    win.document.write('</body></html>');
+    win.document.close();
+    win.print();
   }
 
   deleteAll(index) {
